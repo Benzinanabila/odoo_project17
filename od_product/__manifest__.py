3a716e815492dde5c8b25c031601e4dd108eb96f
Benzinanabila <nabila.benzina27@gmail.com>
2024-09-24 15:15:32 +0100

    [ADD] initial commit for product

---
+++ od_product/__manifest__.py
@@ -0,0 +1,22 @@
+{
+    'name': " Product extension ",
+    'version': '17.0.1.0.0',
+    'category': 'Accounting',
+    'summary': """update products""",
+    'description': """update products""",
+    'author': 'Cybrosys Techno Solutions',
+    'company': 'Cybrosys Techno Solutions',
+    'maintainer': 'Cybrosys Techno Solutions',
+    'website': "https://www.cybrosys.com",
+    'depends': ['product','sale'],
+
+    'data': [
+        'views/product.xml',
+    ],
+
+    'license': 'AGPL-3',
+    'installable': True,
+    'auto_install': False,
+    'application': False,
+}
+
